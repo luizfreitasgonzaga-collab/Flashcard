70d42abeeb7ed82afe6e42bc9f795db911f7762e
luizfreitasgonzaga-collab <luiz.freitas.gonzaga@escola.pr.gov.br>
2025-08-21 11:45:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 3+3?',
+    'A resposta é 6'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é Love'
+)
